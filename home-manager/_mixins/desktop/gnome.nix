d3e0e38d7c63d2736b251c5f1c3acf0449da003f
Joe <hares.melees-0o@icloud.com>
2023-07-30 16:37:55 -0500

    Update home-manager gnome.nix

--- home-manager/_mixins/desktop/gnome.nix
+++ home-manager/_mixins/desktop/gnome.nix
@@ -1,46 +1,46 @@
 { config, lib, pkgs, ... }:
 with lib.hm.gvariant;
 {
-  dconf.settings = {
-    "org/gnome/desktop/datetime" = {
-      automatic-timezone = true;
-    };
-
-    "org/gtk/gtk4/Settings/FileChooser" = {
-      clock-format = "24h";
-    };
-
-    "org/gtk/Settings/FileChooser" = {
-      clock-format = "24h";
-    };
-  };
-  
-  gtk = {
-
-    font = {
-      name = "Work Sans 12";
-      package = pkgs.work-sans;
-    };
-
-    gtk2 = {
-      configLocation = "${config.xdg.configHome}/gtk-2.0/gtkrc";
-      extraConfig = ''
-        gtk-application-prefer-dark-theme=1
-      '';
-    };
-
-    gtk3 = {
-      extraConfig = {
-        gtk-application-prefer-dark-theme = 1;
-      };
-    };
-
-    gtk4 = {
-      extraConfig = {
-        gtk-application-prefer-dark-theme = 1;
-      };
-    };
-
-  };
-
+  #dconf.settings = {
+  #  "org/gnome/desktop/datetime" = {
+  #    automatic-timezone = true;
+  #  };
+#
+  #  "org/gtk/gtk4/Settings/FileChooser" = {
+  #    clock-format = "24h";
+  #  };
+#
+  #  "org/gtk/Settings/FileChooser" = {
+  #    clock-format = "24h";
+  #  };
+  #};
+  #
+  #gtk = {
+#
+  #  font = {
+  #    name = "Work Sans 12";
+  #    package = pkgs.work-sans;
+  #  };
+#
+  #  gtk2 = {
+  #    configLocation = "${config.xdg.configHome}/gtk-2.0/gtkrc";
+  #    extraConfig = ''
+  #      gtk-application-prefer-dark-theme=1
+  #    '';
+  #  };
+#
+  #  gtk3 = {
+  #    extraConfig = {
+  #      gtk-application-prefer-dark-theme = 1;
+  #    };
+  #  };
+#
+  #  gtk4 = {
+  #    extraConfig = {
+  #      gtk-application-prefer-dark-theme = 1;
+  #    };
+  #  };
+#
+  #};
+#
 }
