13f235caf4282d547e644e3e5e747df64bdf2a3e
Joe <hares.melees-0o@icloud.com>
2023-08-01 19:02:33 -0500

    Added nur and firefox extensions

--- nixos/_mixins/desktop/firefox.nix
+++ nixos/_mixins/desktop/firefox.nix
@@ -3,5 +3,12 @@
     enable = true;
     languagePacks = [ "en-US" ];
     package = pkgs.unstable.firefox;
+    extensions = (with pkgs.nur.repos.rycee.firefox-addons; [
+    darkreader
+    privacy-badger
+    ublock-origin
+    bitwarden
+    vimium
+    ]);
   };
 }
